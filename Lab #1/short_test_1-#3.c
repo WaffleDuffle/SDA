a655525d76dfa19fb38234e4c49187099155441d
Constantin Denis <denis.io.constantin@gmail.com>
2023-08-09 23:45:48 +0300

    Create short_test_1-#3.c

---
+++ Lab #1/short_test_1-#3.c
@@ -0,0 +1,16 @@
+/*Realizați un program care să permită alocarea dinamică de memorie pentru un șir S de n caractere, și eliberarea memoriei alocate anterior. Se va afișa adresa de început a șirului atât înainte, cât și după eliberarea memoriei.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    char *S;
+    int n;
+    scanf("%d", &n);
+    S = malloc(n*sizeof(char));
+    printf("\n%p\n", &S);
+    free(S);
+    S = NULL;
+    printf("%p", &S);
+    return 0;
+}
