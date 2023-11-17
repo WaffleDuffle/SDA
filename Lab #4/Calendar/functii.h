894dcbe0b6819221b19efab4d025ca732451b638
Constantin Denis <denis.io.constantin@gmail.com>
2023-11-17 02:13:43 +0200

    Update functii.h

--- Lab #4/Calendar/functii.h
+++ Lab #4/Calendar/functii.h
@@ -10,6 +10,7 @@ int my_strcmp(char*, char*);
 int dimensiune_lista(struct calendar*);
 struct calendar* creare_nod();
 struct calendar* adaugare_sfarsit(struct calendar*);
+struct NOD * stergere_lista(struct NOD *);
 void my_strcpy(char*, char*);
 void afisare_elemente(struct calendar*);
 void afisare_dupa_nume(struct calendar*, char*);
