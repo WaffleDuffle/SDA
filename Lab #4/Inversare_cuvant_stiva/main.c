d1cc7b0ddb4f98f880ee636a496bac929826c173
Constantin Denis <denis.io.constantin@gmail.com>
2023-11-20 21:58:02 +0200

    Add files via upload

---
+++ Lab #4/Inversare_cuvant_stiva/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "functii.h"
+
+int main() {
+    char cuvant[20];
+    scanf("%s", cuvant);
+    struct cuvant* consoane = NULL;
+    struct cuvant* vocale = NULL;
+    for(int i = 0 ; cuvant[i] != '\0' ; ++i){
+        if(cuvant[i] == 'a' || cuvant[i] == 'A' || cuvant[i] == 'e' || cuvant[i] == 'E' ||
+            cuvant[i] == 'i' || cuvant[i] == 'I'  || cuvant[i] == 'o'  || cuvant[i] == 'O' ||
+                cuvant[i] == 'u' || cuvant[i] == 'U'){
+            vocale = push(vocale, cuvant[i]);
+        }
+        else{
+            consoane = push(consoane, cuvant[i]);
+        }
+    }
+    struct cuvant* rez = merge(vocale, consoane);
+    rez = merge(rez, NULL);
+    printf("\n");
+    afisare(rez);
+    return 0;
+}
